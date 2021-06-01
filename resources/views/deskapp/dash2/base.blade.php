149464600778bd1d7b7900af3446adf43194c608
fachriansyahmni <fachriansyahmni@gmail.com>
2021-06-01 12:07:29 +0700

    Add template dashboard 2. Change view home.

---
+++ resources/views/deskapp/dash2/base.blade.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<!-- Basic Page Info -->
+	<meta charset="utf-8">
+	<title>Doger Resto</title>
+
+	<!-- Site favicon -->
+	<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('vendor/deskapp/vendors/images/apple-touch-icon.png') }}">
+	<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('vendor/deskapp/vendors/images/favicon-32x32.png') }}">
+	<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('vendor/deskapp/vendors/images/favicon-16x16.png') }}">
+
+	<!-- Mobile Specific Metas -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<!-- Google Font -->
+	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
+	<!-- CSS -->
+	<link rel="stylesheet" type="text/css" href="{{ asset('vendor/deskapp/vendors/styles/core.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('vendor/deskapp/vendors/styles/icon-font.min.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('vendor/deskapp/src/plugins/datatables/css/dataTables.bootstrap4.min.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('vendor/deskapp/src/plugins/datatables/css/responsive.bootstrap4.min.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('vendor/deskapp/vendors/styles/style.css')}}">
+
+    <style>
+        .header-center{
+            display: flex;
+            width: 50%;
+            align-items: center;
+            align-self: center;
+            place-content: center;
+        }
+        .menu-item{
+            padding: 5px;
+        }
+    </style>
+	@stack('css')
+</head>
+<body>
+	{{-- <div class="pre-loader">
+		<div class="pre-loader-box">
+			<div class="loader-logo"><img src=" {{ asset('/vendor/deskapp/vendors/images/deskapp-logo.svg') }}" alt=""></div>
+			<div class='loader-progress' id="progress_div">
+				<div class='bar' id='bar1'></div>
+			</div>
+			<div class='percent' id='percent1'>0%</div>
+			<div class="loading-text">
+				Harap Tunggu ...
+			</div>
+		</div>
+	</div> --}}
+
+	@include('deskapp.dash2.layouts.header')
+	
+	<!-- js -->
+	<script src="{{ asset('/vendor/deskapp/vendors/scripts/core.js')}}"></script>
+	<script src="{{ asset('/vendor/deskapp/vendors/scripts/script.min.js')}}"></script>
+	<script src="{{ asset('/vendor/deskapp/vendors/scripts/process.js')}}"></script>
+	<script src="{{ asset('/vendor/deskapp/vendors/scripts/layout-settings.js')}}"></script>
+	<script src="{{ asset('/vendor/deskapp/src/plugins/apexcharts/apexcharts.min.js')}}"></script>
+	<script src="{{ asset('/vendor/deskapp/src/plugins/datatables/js/jquery.dataTables.min.js')}}"></script>
+	{{-- <script src="{{ asset('/vendor/deskapp/src/plugins/datatables/js/dataTables.bootstrap4.min.js')}}"></script>
+	<script src="{{ asset('/vendor/deskapp/src/plugins/datatables/js/dataTables.responsive.min.js')}}"></script> --}}
+	<script src="{{ asset('/vendor/deskapp/src/plugins/datatables/js/responsive.bootstrap4.min.js')}}"></script>
+	<script src="{{ asset('/vendor/deskapp/vendors/scripts/dashboard.js')}}"></script>
+
+	@stack('script')
+</body>
+</html>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+{{-- @extends('layouts.app')
 
 @section('content')
 <div class="container">
@@ -20,4 +20,6 @@
         </div>
     </div>
 </div>
-@endsection
+@endsection --}}
+
+@extends('deskapp.dash2.base')
